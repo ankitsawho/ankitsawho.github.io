e8c52f1ff13e3afd41cc171051c85e715d6bfe95
Ankit <ankit1042002@gmail.com>
2023-08-27 22:49:21 +0530

    changed twitter logo

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import {MdLogoDev} from 'react-icons/md'
-import {IoLogoGithub, IoLogoTwitter} from 'react-icons/io'
-import {AiFillYoutube, AiFillLinkedin} from "react-icons/ai"
+import { MdLogoDev } from 'react-icons/md'
+import { IoLogoGithub, IoLogoTwitter } from 'react-icons/io'
+import { AiFillYoutube, AiFillLinkedin } from "react-icons/ai"
 import Link from 'next/link'
+import XLogo from './XLogo'
 
 function SocialLinks() {
-return (
-    <div className="flex items-center justify-start space-x-3 mb-5 mt-4">
-        <Link href="https://github.com/ankitsawho"><IoLogoGithub className=" cursor-pointer" size={28}  /></Link>
-        <Link href="https://dev.to/ankitsahu"><MdLogoDev className=" cursor-pointer" size={28}  /></Link>
-        <Link href="https://www.youtube.com/@ankitsawho"><AiFillYoutube className=" cursor-pointer" size={28} /></Link>
-        <Link href="https://www.linkedin.com/in/ankitsawho/"><AiFillLinkedin className=" cursor-pointer" size={28} /></Link>
-        <Link href="https://twitter.com/ankitsawho"><IoLogoTwitter className=" cursor-pointer" size={28} /></Link>
-    </div>
+    return (
+        <div className="flex items-center justify-start space-x-3 mb-5 mt-4">
+            <Link href="https://github.com/ankitsawho"><IoLogoGithub className=" cursor-pointer" size={30} /></Link>
+            <Link href="https://www.linkedin.com/in/ankitsawho/"><AiFillLinkedin className=" cursor-pointer" size={30} /></Link>
+            <Link href="https://twitter.com/ankitsawho"><XLogo /></Link>
+            <Link href="https://dev.to/ankitsahu"><MdLogoDev className=" cursor-pointer" size={30} /></Link>
+            <Link href="https://www.youtube.com/@ankitsawho"><AiFillYoutube className=" cursor-pointer" size={30} /></Link>
+        </div>
     )
 }
 
